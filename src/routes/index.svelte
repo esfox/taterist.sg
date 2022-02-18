57ca5f2f39194f57a1ff1bc82731701146ae734f
esfox <osairis100@gmail.com>
2022-02-19 07:15:44 +0800

    Add meta tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
 <script context="module">
   /** @type {import('@sveltejs/kit').Load} */
-  export async function load({ fetch }) {
+  export async function load({ fetch, url }) {
     const response = await fetch('menu.json');
     if (response.ok)
       return {
         props: {
           menu: await response.json(),
+          baseUrl: url.origin,
         },
       };
     if (response.status === 404) return { status: 404 };
@@ -21,10 +22,24 @@
   import About from '../components/About.svelte';
 
   export let menu;
+  export let baseUrl;
 </script>
 
 <svelte:head>
   <title>taterist.sg</title>
+  <meta name="description" content="get Baked." />
+
+  <meta property="og:title" content="taterist.sg" />
+  <meta property="og:description" content="get Baked." />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={baseUrl} />
+  <meta property="og:image" content={`${baseUrl}/images/logo.jpg`} />
+
+  <meta property="twitter:card" content="summar" />
+  <meta property="twitter:title" content="taterist.sg" />
+  <meta property="twitter:description" content="get Baked." />
+  <meta property="twitter:url" content={baseUrl} />
+  <meta property="twitter:image" content={`${baseUrl}/images/logo.jpg`} />
 </svelte:head>
 
 <Hero />
