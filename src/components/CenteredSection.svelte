273cba01f76ce26298e7fc1d5a4fb1d95e168d76
esfox <osairis100@gmail.com>
2022-02-18 22:55:26 +0800

    Update hero section

---
+++ src/components/CenteredSection.svelte
@@ -0,0 +1,14 @@
+<script>
+  import classNames from 'classnames';
+</script>
+
+<section class={classNames('grid place-items-center overflow-hidden', $$props.class)}>
+  <div class="text-center">
+    <h1>
+      <slot name="heading" />
+    </h1>
+    <p class="max-w-[500px] mt-2">
+      <slot />
+    </p>
+  </div>
+</section>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,16 +1,17 @@
-<section class="hero h-[200px] md:h-[348px] grid place-items-center text-white overflow-hidden">
-  <div class="text-center">
-    <h1>GET BAKED.</h1>
-    <p class="max-w-[500px] mt-2">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius est vitae efficitur
-      eleifend. Nam tincidunt consectetur luctus.
-    </p>
-  </div>
-</section>
+<script>
+  import CenteredSection from './CenteredSection.svelte';
+</script>
+
+<CenteredSection class="hero h-[240px] md:h-[348px] text-white">
+  <span slot="heading">GET BAKED.</span>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc varius est vitae efficitur eleifend.
+  Nam tincidunt consectetur luctus.
+</CenteredSection>
 
 <style>
-  .hero {
-    background: linear-gradient(#00000070, #00000070), url('/images/hero-bg.jpg');
+  :global(.hero) {
+    background: linear-gradient(var(--color-bg-overlay-dark), var(--color-bg-overlay-dark)),
+      url('/images/hero-bg.jpg');
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
