35a8240d2ae26afe262b8b31f0108a3e7aa08181
esfox <osairis100@gmail.com>
2022-02-22 22:12:57 +0800

    Change adapter to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import WindiCSS from 'vite-plugin-windicss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
     vite: {
       plugins: [
         WindiCSS(),
